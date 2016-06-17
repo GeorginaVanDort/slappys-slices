f0d9e6837c1eab3baee770d9199af591ee753c75
Georgina Van Dort <GeorginavanDort@gmail.com>
2016-06-17 14:33:05 -0700

    have basic function working

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,10 @@ function PizzaOrder(size) {
   this.special = [];
 }
 
+PizzaOrder.prototype.protein = function(protein) {
+  this.protein.push(protein);
+}
+
 PizzaOrder.prototype.price = function () {
   return this.size + (this.protein.length * 2);
 }
@@ -12,20 +16,23 @@ PizzaOrder.prototype.price = function () {
 
 
 $(document).ready(function() {
-  event.preventDefault();
+  $("form#newPizza").submit(function(event) {
+    event.preventDefault();
 
-  $("#size").change(function() {
     var size = parseInt($('#size input:radio:checked').val());
-  })
+    var protein = $("#protein1").val();
+    var veg = $("#veg").val();
+    var special = $("#special").val();
 
-  var newPizza = new PizzaOrder(size);
-
-  $(".protein").change(function() {
-    var protein = $(this).val();
+    var newPizza = new PizzaOrder(size);
     newPizza.protein.push(protein);
+    newPizza.veg.push(veg);
+    newPizza.special.push(special);
+
+    $("#total").text(newPizza.price());
+
   })
 
-  $("#total").text(newPizza.price());
 })
 
 
@@ -39,7 +46,13 @@ $(document).ready(function() {
 
 
 
-
+  // var newPizza = new PizzaOrder(size);
+// // debugger;
+//   $(".protein").change(function() {
+//     var protein = $(this).val();
+//     alert(protein);
+//     // newPizza.protein.push(protein);
+//   })
 // var protein = $("#protein").val();
 // var veg = $("#veg").val();
 // var special = $("#special").val();
